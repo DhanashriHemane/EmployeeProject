d0058d7e5bae3ace74989d50d0164201c4d0e75e
dhanashrihemane78@gmail.com <dhanashrihemane78@gmail.com>
2021-08-05 17:06:25 +0530

    Added: Code of Calculation of Employee daily wege

--- Employee.sh
+++ Employee.sh
@@ -1,10 +1,15 @@
 
 echo "Welcome to Employee Wage Computation"
 randomNumber=$(($RANDOM%2))
-
+salaryPerHr=20
+totalSalary=0
 if [ $randomNumber -eq 1 ]
 then
 	echo "Present"
+	workHr=8
 else
 	echo "Absent"
+	workHr=0
 fi
+totalSalary=$((salaryPerHr*workHr))
+echo "Daily employee wege: " $totalSalary
