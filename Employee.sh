29633cba2041d6fc3e21a60230a85505b87b656e
dhanashrihemane78@gmail.com <dhanashrihemane78@gmail.com>
2021-08-05 17:18:40 +0530

    Added: Code of Part time EMP wege

--- Employee.sh
+++ Employee.sh
@@ -1,15 +1,19 @@
 
 echo "Welcome to Employee Wage Computation"
-randomNumber=$(($RANDOM%2))
+randomNumber=$(($RANDOM%3))
 salaryPerHr=20
 totalSalary=0
 if [ $randomNumber -eq 1 ]
 then
 	echo "Present"
 	workHr=8
+elif [ $randomNumber -eq 2 ]
+then 
+	echo "Part Time"
+	workHr=8
 else
 	echo "Absent"
 	workHr=0
 fi
 totalSalary=$((salaryPerHr*workHr))
-echo "Daily employee wege: " $totalSalary
+echo "Employee wege: " $totalSalary
